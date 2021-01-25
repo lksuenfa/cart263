e4f4265b385e6661745a48be289f2399f5d363b6
Leanne Suen Fa <70963322+lksuenfa@users.noreply.github.com>
2021-01-25 10:09:48 -0500

    A1: set up files

---
+++ Activities/wk1_sausage dog/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**************************************************
+Where is sausage dog?
+CART263 week 1
+Leanne Suen Fa
+
+Find the sausage dog.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}
