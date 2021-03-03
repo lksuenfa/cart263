8920f15b5e0263f416b3861256e323603b2fb868
lksuenfa <70963322+lksuenfa@users.noreply.github.com>
2021-03-03 17:50:00 -0500

    P1: finish adding all effects on water in glass.js

--- Projects/midterm/js/Glass.js
+++ Projects/midterm/js/Glass.js
@@ -2,7 +2,7 @@ class Glass {
   constructor() {
     this.glass = {
       x: width / 2 - 100,
-      y: 100,
+      y: 150,
       width: 150,
       height: 500,
       colour: {
@@ -14,13 +14,20 @@ class Glass {
     };
 
     this.water = {
+      x: this.glass.x + 20,
+      y: this.glass.y + 110,
+      width: this.glass.width * 0.75,
+      height: this.glass.height * 0.75,
       colour: {
         r: 132,
         g: 186,
         b: 227,
         a: 200,
       },
+      vy: 1,
     };
+
+    this.impurities = 100;
   }
 
   //change colour of water
@@ -28,6 +35,27 @@ class Glass {
     this.water.colour.r = random(100, 255);
     this.water.colour.g = random(100, 255);
     this.water.colour.b = random(100, 255);
+  }
+
+  //increase water
+  increase() {
+    this.water.height += this.water.vy;
+    this.water.y -= this.water.vy;
+  }
+
+  //make water disappear by turning it transparent
+  disappear() {
+    this.water.colour.a = 0;
+  }
+
+  //make impurities appear in water
+  impurities() {
+    for (let i = 0; i < this.impurities; i++) {
+      let x = random(this.water.x, this.water.x + this.water.width);
+      let y = random(this.water.y, this.water.y + this.water.height);
+      stroke(255);
+      point(x, y);
+    }
   }
 
   display() {
@@ -53,12 +81,7 @@ class Glass {
       this.water.colour.b,
       this.water.colour.a
     );
-    rect(
-      this.glass.x + 20,
-      this.glass.y + 110,
-      this.glass.width * 0.75,
-      this.glass.height * 0.75
-    );
+    rect(this.water.x, this.water.y, this.water.width, this.water.height);
     pop();
   }
 }
