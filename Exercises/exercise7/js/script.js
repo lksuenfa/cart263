e88c6be17d2ef8ab06b0d0b78b65baf14f2c067d
lksuenfa <70963322+lksuenfa@users.noreply.github.com>
2021-03-25 20:52:38 -0400

    Duplicate activity and change colour + add modal dialogue

--- Exercises/exercise7/js/script.js
+++ Exercises/exercise7/js/script.js
@@ -8,19 +8,6 @@ author, and this description to match your project!
 
 "use strict";
 
-// modal dialog at the start of the game
-$(`#introduction-dialog`).dialog({
-  modal: true,
-
-  // click button to close modal and start
-  buttons: {
-    Start: function () {
-      $(this).dialog(`close`);
-    },
-  },
-});
-
-//
 $(`#solved-dialog`).dialog({
   autoOpen: false,
   buttons: {
