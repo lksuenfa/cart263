d19c100fcfd9bb590625a8fc7d59676f004d0ad2
lksuenfa <70963322+lksuenfa@users.noreply.github.com>
2021-03-29 15:01:33 -0400

    P1: generate a random blood sugar value between 1 and 15

--- Projects/final proposal/js/script.js
+++ Projects/final proposal/js/script.js
@@ -6,6 +6,3 @@ Navigate through the basic steps of managing variations in blood sugar level
 */
 
 "use strict";
-
-// <button onclick="/simulation.html">Next</button>;
-
--- Projects/final proposal/js/simulation.js
+++ Projects/final proposal/js/simulation.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let bloodSugar = document.getElementById("blood-sugar");
+
+// display random blood sugar
+bloodSugar.innerHTML = " Blood sugar : " + randomIntFromInterval(1, 15);
+
+// Generate a random number and round it up
+function randomIntFromInterval(min, max) {
+  // Generate a random number between intervals https://gist.github.com/spyesx/485e4584aae767201f41
+  let randomNum = Math.random() * (max - min + 1) + min;
+  // round the answer to 1 decimal place https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+  let roundedNum = Math.round(randomNum * 10) / 10;
+  return roundedNum;
+}
