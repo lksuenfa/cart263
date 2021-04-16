0b90e979a62b498abaaacea9cab6cc5bd386ce52
lksuenfa <70963322+lksuenfa@users.noreply.github.com>
2021-04-16 16:24:02 -0400

    P2: change from p5 to Phaser

---
+++ Projects/final/js/main.js
@@ -0,0 +1,19 @@
+/**************************************************
+Hypoglycemia simulation
+Help Oscar stay alive
+Leanne Suen Fa
+
+Select the proper items in the correct order according to the random blood sugar reading
++ Set up using Phaser 3
+**************************************************/
+
+"use strict";
+
+let config = {
+  type: Phaser.AUTO,
+  width: 1000,
+  height: 700,
+  scene: [],
+};
+
+let game = new Phaser.Game(config);
